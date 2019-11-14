351360a0f83aacc69fb5ba36b92e7bd62632d2a2
Zuodian Hu <huzuodian@gmail.com>
2019-11-13 21:31:42 -0600

    fix(test): correcly use random generators

--- test/test_span.cpp
+++ test/test_span.cpp
@@ -13,16 +13,16 @@
 TEMPLATE_TEST_CASE_SIG(
     "span constructed from various sources have correct data and size",
     "[span][ctor][data][size]",
-    ( ( typename T , std::size_t Extent , typename TestType          ), T, Extent, TestType ),
-    (   int        ,                   8, int[8]                     ),
-    (   double     ,                  16, double[16]                 ),
-    (   std::string,                  12, std::string[12]            ),
-    (   int        ,                   4, std::array<int,4>          ),
-    (   double     ,                   7, std::array<double,7>       ),
-    (   std::string,                  12, std::array<std::string,12> ),
-    (   int        , gsl::dynamic_extent, std::vector<int>           ),
-    (   double     , gsl::dynamic_extent, std::vector<double>        ),
-    (   std::string, gsl::dynamic_extent, std::vector<std::string>   )
+    ( ( typename T , std::size_t Extent ,   typename TestType            ), T, Extent, TestType ),
+    (   int        ,                   8,   int[8]                       ),
+    (   double     ,                  16,   double[16]                   ),
+    (   std::string,                  12,   std::string[12]              ),
+    (   int        ,                   4, ( std::array<int,4>          ) ),
+    (   double     ,                   7, ( std::array<double,7>       ) ),
+    (   std::string,                  12, ( std::array<std::string,12> ) ),
+    (   int        , gsl::dynamic_extent,   std::vector<int>             ),
+    (   double     , gsl::dynamic_extent,   std::vector<double>          ),
+    (   std::string, gsl::dynamic_extent,   std::vector<std::string>     )
 ) {
     SECTION( "default-constructed span" ) {
         gsl::span<TestType> dyn_ext_span;
@@ -37,7 +37,7 @@ TEMPLATE_TEST_CASE_SIG(
     SECTION( "pointer-count-constructed span" ) {
         TestType test_data;
         if constexpr ( Extent == gsl::dynamic_extent ) {
-            const auto num_elems = GENERATE( random<gsl::index>( 0, 32 ) );
+            const auto num_elems = GENERATE( take( 1, random<gsl::index>( 0, 32 ) ) );
 
             for ( gsl::index i = 0; i < num_elems; ++i ) {
                 test_data.push_back( T{} );
@@ -56,7 +56,7 @@ TEMPLATE_TEST_CASE_SIG(
     SECTION( "pointer-pair-constructed span" ) {
         TestType test_data;
         if constexpr ( Extent == gsl::dynamic_extent ) {
-            auto num_elems = GENERATE( random<gsl::index>( 0, 32 ) );
+            auto num_elems = GENERATE( take( 1, random<gsl::index>( 0, 32 ) ) );
 
             for ( gsl::index i = 0; i < num_elems; ++i ) {
                 test_data.push_back( T{} );
@@ -65,7 +65,7 @@ TEMPLATE_TEST_CASE_SIG(
 
         gsl::span<T> dyn_ext_span(
             std::data( test_data ),
-            std::data( test_data ) + std::size( test_data )
+            std::data( test_data ) + std::size( test_data ) - 1
         );
         CHECK( dyn_ext_span.data() == std::data( test_data ) );
         CHECK( dyn_ext_span.size() == std::size( test_data ) );
@@ -81,7 +81,7 @@ TEMPLATE_TEST_CASE_SIG(
     SECTION( "container-constructed span" ) {
         TestType test_data;
         if constexpr ( Extent == gsl::dynamic_extent ) {
-            auto num_elems = GENERATE( random<gsl::index>( 0, 32 ) );
+            auto num_elems = GENERATE( take( 1, random<gsl::index>( 0, 32 ) ) );
 
             for ( gsl::index i = 0; i < num_elems; ++i ) {
                 test_data.push_back( T{} );
