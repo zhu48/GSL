0cb7dc767a296e183819081abdd1491d1eba4b9f
Zuodian Hu <huzuodian@gmail.com>
2019-11-15 17:44:20 -0600

    feat(assert): Expects and Ensures

---
+++ include/gsl_assert.hpp
@@ -0,0 +1,9 @@
+#ifndef GSL_ASSERT_HPP
+#define GSL_ASSERT_HPP
+
+#include <cassert>
+
+#define Expects( condition ) assert( condition )
+#define Ensures( condition ) assert( condition )
+
+#endif // #ifndef GSL_ASSERT_HPP
