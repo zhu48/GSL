00347fddfaf05d440a7635f56e5f3e0767adc7cd
Zuodian Hu <huzuodian@gmail.com>
2019-11-17 07:59:01 -0600

    feat(docs): document gsl::at() public interface

--- include/gsl_util.hpp
+++ include/gsl_util.hpp
@@ -34,18 +34,50 @@ namespace gsl {
         return static_cast<N>( std::forward<W>( val ) );
     }
 
+    /**
+     * Checked indexed built-in array access.
+     * 
+     * \tparam T The type of the array elements.
+     * \tparam N The number of elements in the array.
+     * 
+     * \param arr The array to access an element of.
+     * 
+     * \param i The index of the array element to access.
+     * 
+     * \return Returns the element to be accessed.
+     */
     template<typename T, std::size_t N>
     constexpr T& at( T ( &arr )[N], index i ) {
         Expects( i >= 0 && i < narrow_cast<index>( N ) );
         return arr[i];
     }
 
+    /**
+     * Checked indexed container access.
+     * 
+     * \tparam Container The type of container to access.
+     * 
+     * \param cont The container to access an element of.
+     * \param i    The index of the element to access.
+     * 
+     * \return Returns the element to be accessed.
+     */
     template<typename Container>
     constexpr auto at( Container& cont, index i ) -> decltype( cont[cont.size()] ) {
         Expects( i >= 0 && i < detail::ssize( cont ) );
         return cont[i];
     }
 
+    /**
+     * Checked indexed initializer list access.
+     * 
+     * \tparam T The type of object the initializer list contains.
+     * 
+     * \param cont The initializer list to access an element of.
+     * \param i    The index of the initializer list element to access.
+     * 
+     * \return Returns the element to be accessed.
+     */
     template<typename T>
     constexpr T at( const std::initializer_list<T> cont, index i ) {
         Expects( i >= 0 && i < detail::ssize( cont ) );
